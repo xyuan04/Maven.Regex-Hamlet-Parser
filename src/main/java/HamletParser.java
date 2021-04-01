cdc2ab8ad0aa70a75d0b6596db609b9c775a371d
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-03-31 20:48:32 -0400

    finished lab

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -34,6 +36,61 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String changeHamletToLeon() {
+        Pattern pattern = Pattern.compile("[hH]amlet");
+        Matcher matcher = pattern.matcher(hamletData);
+        boolean matchFound = matcher.find();
+        String newPoem = hamletData;
+
+        if(matchFound) {
+            newPoem = matcher.replaceAll("Leon");
+        }
+
+        Pattern patternCaps = Pattern.compile("HAMLET");
+        Matcher matcherCaps = patternCaps.matcher(newPoem);
+        boolean matchFoundCaps = matcherCaps.find();
+
+        if(matchFoundCaps) {
+            newPoem = matcherCaps.replaceAll("LEON");
+        }
+
+        return newPoem;
+    }
+
+    public String changeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("[hH]oratio");
+        Matcher matcher = pattern.matcher(hamletData);
+        boolean matchFound = matcher.find();
+        String newPoem = hamletData;
+
+        if(matchFound) {
+            newPoem = matcher.replaceAll("Tariq");
+        }
+
+        Pattern patternCaps = Pattern.compile("HORATIO");
+        Matcher matcherCaps = patternCaps.matcher(newPoem);
+        boolean matchFoundCaps = matcherCaps.find();
+
+        if(matchFoundCaps) {
+            newPoem = matcherCaps.replaceAll("TARIQ");
+        }
+
+        return newPoem;
+    }
+
+    public boolean findHamlet(String input) {
+        Pattern pattern = Pattern.compile("hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+    public boolean findHoratio(String input) {
+        Pattern pattern = Pattern.compile("horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.find();
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,139 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHamletToLeon();
+
+        //Then:
+        Assert.assertFalse(actual.contains("Hamlet"));
+    }
+
+    @Test
+    public void testChangeHamletToLeonContainsLeon() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHamletToLeon();
+
+        //Then:
+        Assert.assertTrue(actual.contains("Leon"));
+    }
+
+    @Test
+    public void testChangeHamletToLeonCaps() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHamletToLeon();
+
+        //Then:
+        Assert.assertFalse(actual.contains("HAMLET"));
+    }
+
+    @Test
+    public void testChangeHamletToLeonContainsLeonCaps() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHamletToLeon();
+
+        //Then:
+        Assert.assertTrue(actual.contains("LEON"));
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHoratioToTariq();
+
+        //Then:
+        Assert.assertFalse(actual.contains("Horatio"));
+    }
+
+    @Test
+    public void testChangeHoratioToTariqContainsTariq() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHoratioToTariq();
+
+        //Then:
+        Assert.assertTrue(actual.contains("Tariq"));
+    }
+
+    @Test
+    public void testChangeHoratioToTariqCaps() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHoratioToTariq();
+
+        //Then:
+        Assert.assertFalse(actual.contains("HORATIO"));
+    }
+
+    @Test
+    public void testChangeHoratioToTariqContainsTariqCaps() {
+        //Given:
+
+        //When:
+        String actual = hamletParser.changeHoratioToTariq();
+
+        //Then:
+        Assert.assertTrue(actual.contains("TARIQ"));
     }
 
     @Test
     public void testFindHoratio() {
+        //Given:
+
+        //When:
+        boolean expected = true;
+        boolean actual = hamletParser.findHoratio(hamletParser.getHamletData());
+
+        //Then:
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHoratioFalse() {
+        //Given:
+
+        //When:
+        boolean expected = false;
+        String changedPoem = hamletParser.changeHoratioToTariq();
+        boolean actual = hamletParser.findHoratio(changedPoem);
+
+        //Then:
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        //Given:
+
+        //When:
+        boolean expected = true;
+        boolean actual = hamletParser.findHamlet(hamletParser.getHamletData());
+
+        //Then:
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHamletFalse() {
+        //Given:
+
+        //When:
+        boolean expected = false;
+        String changedPoem = hamletParser.changeHamletToLeon();
+        boolean actual = hamletParser.findHamlet(changedPoem);
+
+        //Then:
+        Assert.assertEquals(expected, actual);
     }
 }
